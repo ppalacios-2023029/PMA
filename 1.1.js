afe83619ea2e454ef691264ae0d1c306f0e89ae2
ppalacios-2023029 <ppalacios-2023029@kinal.edu.gt>
2025-03-06 17:30:12 -0600

    1.1

---
+++ 1.1.js
@@ -0,0 +1,11 @@
+const product = {
+    name: 'Laptop Asus',
+    precio: 1500,
+    disponible: true
+}
+
+descuento = product.precio * 0.10
+total = product.precio - descuento
+
+console.log(product)
+console.log(total)
